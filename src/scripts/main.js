d66a3add6532277b5ba17685db3720a8ab204884
lndexNull <nullablelndex@gmail.com>
2023-03-07 19:45:54 +0100

    some more style stuff

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,3 +14,16 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 3000, fill: "forwards" });
+}
